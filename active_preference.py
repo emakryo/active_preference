fad281845f739c2edfae1fcd46b3521b28333aff
Kamesawa Ryosuke <emak.ryo@gmail.com>
2016-06-22 15:19:28 +0900

    drivation wrt gamma

--- active_preference.py
+++ active_preference.py
@@ -1,21 +1,21 @@
 import numpy as np
 import scipy as sp
 from scipy.stats import norm
-from sklearn.metrics.pairwise import pairwise_kernels
+from sklearn.metrics.pairwise import pairwise_kernels, euclidean_distances
 
 class ActivePreference():
     def __init__(self, bound):
         self._bound = np.array(bound)
         assert len(self._bound.shape) == 2
         assert self._bound.shape[1] == 2
-        self._x = [self._bound.mean(axis=1)]
-        self.dim = self._bound.shape[0]
+        self._x = np.array([self._bound.mean(axis=1)])
+        self._ndim = self._bound.shape[0]
         self._i = []
         self._j = []
         self._last_query = None
         self._sgm = 1.0
 
-        self._kernel_fun = lambda x,y=None: pairwise_kernels(x,y,metric='rbf',gamma=10)
+        self._kernel = RBF(self._x, gamma=10/self._ndim)
 
     def query(self):
         pass
@@ -40,27 +40,48 @@ class ActivePreference():
         if q not in self.x:
             self._x.append(q)
 
-        self._i.append(_x.index(p))
-        self._j.append(_x.index(q))
+        self._i.append(self._x.index(p))
+        self._j.append(self._x.index(q))
 
-        self._kernel = self._kernel_fun(self._x)
+        self._kernel.X = self._x
 
+    @property
+    def x(self):
+        return self._x
+
+    @x.setter
+    def x(self, x):
+        self._x = np.array(x)
+        self._kernel.X = self._x
+        self._maximize_posterior()
+
+    @property
+    def lng(self):
+        return np.log(self._kernel.gamma)
+
+    @lng.setter
+    def lng(self, lng):
+        self._kernel.gamma = np.exp(lng)
 
     def best(self):
         pass
 
-    def _log_likelihood(self,f):
+    def _log_likelihood(self, f=None):
+        if f is None: f = self._fMAP
         zk = (f[np.array(self._i)]-f[np.array(self._j)])/(np.sqrt(2)*self._sgm)
         return np.sum(np.log(norm.cdf(zk)))
 
-    def _log_prior(self,f):
-        alpha = np.linalg.solve(np.linalg.cholesky(self._kernel),f)
+    def _log_prior(self, f=None):
+        if f is None: f = self._fMAP
+        alpha = np.linalg.solve(np.linalg.cholesky(self._kernel()),f)
         return -0.5*np.sum(alpha**2)
 
-    def _log_posterior(self, f):
+    def _log_posterior(self, f=None):
+        if f is None: f = self._fMAP
         return self._log_likelihood(f) + self._log_prior(f)
 
-    def _d_log_likelihood(self, f):
+    def _d_log_likelihood(self, f=None):
+        if f is None: f = self._fMAP
         k = len(self._i)
         zk = (f[np.array(self._i)]-f[np.array(self._j)])/(np.sqrt(2)*self._sgm)
         sik = np.zeros((len(f),k))
@@ -68,13 +89,16 @@ class ActivePreference():
         sik[self._j,np.arange(k)] = -1/np.sqrt(2)*self._sgm
         return np.matmul(sik, norm.pdf(zk)/norm.cdf(zk))
 
-    def _d_log_prior(self, f):
-        return -np.linalg.solve(self._kernel,f)
+    def _d_log_prior(self, f=None):
+        if f is None: f = self._fMAP
+        return -np.linalg.solve(self._kernel(),f)
 
-    def _d_log_posterior(self, f):
+    def _d_log_posterior(self, f=None):
+        if f is None: f = self._fMAP
         return self._d_log_likelihood(f) + self._d_log_prior(f)
 
-    def _dd_log_likelihood(self, f):
+    def _dd_log_likelihood(self, f=None):
+        if f is None: f = self._fMAP
         k = len(self._i)
         zk = (f[np.array(self._i)]-f[np.array(self._j)])/(np.sqrt(2)*self._sgm)
         sik = np.zeros((len(f),k))
@@ -85,18 +109,106 @@ class ActivePreference():
         cdf = norm.cdf(zk)
         return np.tensordot(sijk, -pdf**2/cdf**2-zk*pdf/cdf,axes=([2],[0]))
 
-    def _dd_log_prior(self, f):
-        return np.linalg.inv(self._kernel)
+    def _dd_log_prior(self, f=None):
+        if f is None: f = self._fMAP
+        return -np.linalg.inv(self._kernel())
 
-    def __dd_log_posterior(self, f):
+    def _dd_log_posterior(self, f=None):
+        if f is None: f = self._fMAP
         return self._dd_log_likelihood(f) + self._dd_log_prior(f)
 
-    def _maximize_posterior(self):
+    def _argmax_posterior(self):
         def obj(f):
-            return f - np.matmul(self._kernel, self._d_log_likelihood(f))
+            return f - np.matmul(self._kernel(), self._d_log_likelihood(f))
 
         def dobj(f):
             return np.identity(len(f)) -\
-                np.matmul(self._kernel, self._dd_log_likelihood(f))
+                np.matmul(self._kernel(), self._dd_log_likelihood(f))
 
         return sp.optimize.fsolve(obj, np.zeros(len(self._x)), fprime=dobj)
+
+    def _maximize_posterior(self):
+        self._fMAP = self._argmax_posterior()
+
+    def _log_marginal_likelihood(self, lns, lng):
+        self.lng = lng
+        self._sgm = np.exp(lns)
+        self._maximize_posterior()
+        m = np.identity(len(self._x))+np.matmul(self._kernel(),-self._dd_log_likelihood())
+        s = -self._log_posterior()
+        t = -0.5*np.log(np.linalg.det(m))
+        return s+t
+
+    def _d_log_marginal_likelihood_lng(self, lns, lng):
+        self.lng = lng
+        self._sgm = np.exp(lns)
+        self._maximize_posterior()
+        alpha = np.linalg.solve(self._kernel(), self._fMAP)
+        dg = self._kernel.d_gamma()
+        lam = -self._dd_log_likelihood()
+        m = np.linalg.solve(np.identity(len(self._x))+
+                            np.matmul(self._kernel(),lam),
+                            np.matmul(dg,lam))
+        s = 0.5*np.exp(lng)*np.dot(alpha, np.matmul(dg,alpha))
+        t = -0.5*np.exp(lng)*np.trace(m)
+        return s+t
+
+    def _d_log_marginal_likelihood_lns(self, lns, lng):
+        self._kernel.gamma = np.exp(lng)
+        self._sgm = np.exp(lns)
+        self._maximize_posterior()
+        zk = (self._fMAP[np.array(self._i)]-self._fMAP[np.array(self._j)])/(np.sqrt(2)*self._sgm)
+        return self._sgm*(np.sum(dzk_ds * pdf) - 0.5*np.trace())
+
+    def _d_log_likelihood_lns(self):
+        return
+
+    def _argmax_marginal_likelihood(self):
+        pass
+
+    def _expected_improvement(self,x):
+        pass
+
+    def _argmax_expected_improvement(self):
+        pass
+
+
+class RBF():
+    def __init__(self, X, Y=None, gamma=None):
+        self._gamma = gamma
+        self._X = X
+        self._Y = Y
+        self._K = self(self._X, self._Y, self._gamma)
+
+    @property
+    def X(self):
+        return self._X
+    @property
+    def Y(self):
+        return self._Y
+    @property
+    def gamma(self):
+        return self._gamma
+
+    @X.setter
+    def X(self, X):
+        self._K = self(X=X)
+    @Y.setter
+    def Y(self, Y):
+        self._K = self(Y=Y)
+    @X.setter
+    def gamma(self, gamma):
+        self._K = self(gamma=gamma)
+
+    def __call__(self, X=None, Y=None, gamma=None):
+        if X is None and Y is None and gamma is None: return self._K
+        if X is not None: self._X = X
+        if Y is not None: self._Y = Y
+        if gamma is not None: self._gamma = gamma
+        self._ssd = euclidean_distances(self._X,self._Y,squared=True)
+        self._K = pairwise_kernels(self._X, self._Y, metric='rbf', gamma=self._gamma)
+        return self._K
+
+    def d_gamma(self, gamma=None):
+        self._K = self(gamma=gamma)
+        return -self._ssd*self._K
--- test.py
+++ test.py
@@ -12,14 +12,14 @@ def gaus_mix(x):
 def test():
     model = ActivePreference([[0,1]])
 
-    x = list(np.linspace(0,1,5).reshape(-1,1))
+    x = np.linspace(0,1,5).reshape(-1,1)
     i = []
     j = []
     def v(x):
         if x.ndim==2:
-            return -x[:,0]
+            return x[:,0]
         else:
-            return -x[0]
+            return x[0]
 
     v = gaus_mix
 
@@ -31,31 +31,46 @@ def test():
             i.append(k+1)
             j.append(k)
 
-    model._x = x
     model._i = i
     model._j = j
+    model.x = x
 
-    model._kernel = model._kernel_fun(np.array(model._x).reshape(-1,1))
+#    f = np.zeros((5,5))
+#    f[:,0] = np.arange(-2,3)
+#
+#    dpost = np.array([model._d_log_posterior(f[i])[0] for i in range(5)])
+#
+#    g = np.zeros((4,5))
+#    g[:,0] = np.arange(-1.5,2)
+#
+#    ddpost = [model._dd_log_posterior(g[i])[0,0] for i in range(4)]
+#
+#    plt.plot(g[:,0], dpost[1:]-dpost[:-1])
+#    plt.plot(g[:,0], ddpost, "x")
+#    #plt.show()
+#    plt.clf()
+#
+#    fopt = model._argmax_posterior()
+#    x = x.flatten()
+#    plt.plot(x,fopt)
+#    plt.plot(x,v(x.reshape(-1,1)))
+#    plt.show()
 
-    f = np.zeros((5,5))
-    f[:,0] = np.arange(-2,3)
+    lng0 = np.linspace(0,5,10)
+    lng1 = (lng0[1:]+lng0[:-1])/2
+    ml = np.array([model._log_marginal_likelihood(1,y) for y in lng0])
+    dml = np.array([model._d_log_marginal_likelihood_lng(1,y) for y in lng1])
 
-    dpost = np.array([model._d_log_likelihood(f[i])[0] for i in range(5)])
-
-    g = np.zeros((4,5))
-    g[:,0] = np.arange(-1.5,2)
-
-    ddpost = [model._dd_log_likelihood(g[i])[0,0] for i in range(4)]
-
-    plt.plot(g[:,0], dpost[1:]-dpost[:-1])
-    plt.plot(g[:,0], ddpost, "x")
-    plt.show()
     plt.clf()
+    plt.plot(lng0,ml)
+    plt.show()
 
-    fopt = model._maximize_posterior()
-    x = np.array(x).flatten()
-    plt.plot(x,fopt)
-    plt.plot(x,v(x.reshape(-1,1)))
+    print(ml)
+    print(dml)
+
+    plt.clf()
+    plt.plot(lng1, ml[1:]-ml[:-1])
+    plt.plot(lng1, dml)
     plt.show()
 
 
